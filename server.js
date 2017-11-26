185bc0e10aa3e78d7835bb2a030111b32d94fcdb
Shachy Rivas <shachyrivas@Shachys-Air.home>
2017-11-26 04:32:00 -0500

    Add login, register routes

--- server.js
+++ server.js
@@ -1,14 +1,20 @@
 const express = require('express');
-const fs = require('fs');
 const path = require('path');
+const bodyParser = require('body-parser');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+const UserController = require('./controllers/UserController.js');
+
+app.use(bodyParser.json());
 app.use(express.static('./bundle'));
+
+app.post('/register', UserController.add);
+app.post('/login', UserController.verify);
 
 app.get('*', (req, res) => res.sendFile(path.join(__dirname, 'index.html')));
 
 app.listen(PORT, () => {
-  console.log(`test-ssr16 app is listening on ${PORT}`);
+  console.log(`Login with react application is listening on ${PORT}`);
 });
