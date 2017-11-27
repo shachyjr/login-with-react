26d5b21baf7768caa52ac24338dea1640cce9d96
Shachy Rivas <shachyrivas@Shachys-MacBook-Air.local>
2017-11-27 13:28:39 -0500

    Handle calls to server to validate use login information Handle redirecting using withRouter and the history prop for login and logout handlers

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,10 @@
 import React, { Component } from 'react';
-import { Route, Switch } from 'react-router-dom';
+import { Route, Switch, withRouter } from 'react-router-dom';
 import Login from './pages/Login';
 import Register from './pages/Register';
+import Home from './pages/Home';
 import NotFound from './pages/NotFound';
+import PrivateRoute from './PrivateRoute';
 
 class App extends Component {
   constructor() {
@@ -10,36 +12,54 @@ class App extends Component {
     this.state = {
       username: '',
       password: '',
+      user: null,
     };
     this.handleLogin = this.handleLogin.bind(this);
     this.handleUNChange = this.handleUNChange.bind(this);
     this.handlePWChange = this.handlePWChange.bind(this);
+    this.handleLogout = this.handleLogout.bind(this);
   }
 
   handleLogin(event) {
-    // event.preventDefault();
+    event.preventDefault();
     console.log(`username: ${this.state.username} \npassword: ${this.state.password}`);
+    const http = new XMLHttpRequest();
+    http.open('POST', '/login', true);
+    http.onreadystatechange = () => {
+      if (http.readyState === 4 && http.status === 200) {
+        console.log(http.responseText);
+        this.setState({ user: http.responseText });
+        this.props.history.push('/home');
+      }
+    };
+    http.setRequestHeader('Content-Type', 'application/json');
+    http.send(JSON.stringify({ username: this.state.username, password: this.state.password }));
   }
 
   handleUNChange(event) {
-    console.log('un: ', event.target.value);
     this.setState({ username: event.target.value });
   }
 
   handlePWChange(event) {
-    console.log('pw: ', event.target.value);
     this.setState({ password: event.target.value });
+  }
+
+  handleLogout() {
+    console.log("INSIDE HANDLE LOGOUT");
+    this.setState({ user: null });
+    this.props.history.push('/');
   }
 
   render() {
     return [
       <Switch key="router">
-        <Route exact path="/" render={() => <Login handleLogin={this.handleLogin} handleUNChange={this.handleUNChange} handlePWChange={this.handlePWChange} />} />
+        <Route exact path="/" render={() => <Login handleLogin={this.handleLogin} handleUNChange={this.handleUNChange} handlePWChange={this.handlePWChange} user={this.state.user}/>} />
         <Route path="/register" render={() => <Register />} />
+        <PrivateRoute path="/home" component={Home} user={this.state.user} handleLogout={this.handleLogout}/>
         <Route path="/*" render={() => <NotFound />} />
       </Switch>,
     ];
   }
 }
 
-export default App;
+export default withRouter(App);
